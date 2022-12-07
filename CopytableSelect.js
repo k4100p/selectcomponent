9761765436b2624b3069a0e9159577fe3599df8f
k4100p <kamwanlai@gmail.com>
2022-12-07 16:40:03 +0800

    Initial commit

---
+++ CopytableSelect.js
@@ -0,0 +1,15 @@
+import { ProFormSelect } from "@ant-design/pro-components";
+import React from "react";
+
+export default (arg1) => {
+  console.log(arg1);
+  // const {_, { defaultRender } } = {...props};
+  // return <ProFormSelect {...arg2} />;
+  return <></>;
+};
+
+// export default (_, { defaultRender }) => {
+//   console.log(_);
+//   // console.log(arguments);
+//   return defaultRender(_);
+// };
